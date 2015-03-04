209482ec2559fa4fd551ad7f2eb5a73994820399
Christopher Mamian <christophermamian@gmail.com>
2015-03-04 11:48:13 -0800

    basic html page

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 var express = require('express'),
 	app = express(),
 	server = require('http').createServer(app),
-	io = require9'socket.io').listen(server);
+	io = require('socket.io').listen(server);
 
 server.listen(3000);
 
+app.get('/', function(req, res){
+	res.sendfile(__dirname + '/index.html');
+});
